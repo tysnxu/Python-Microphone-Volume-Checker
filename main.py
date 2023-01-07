4b64a1b6874a67143ba0bc67c11b3a98d5fefd6b
tysnxu <tysn.xu@gmail.com>
2023-01-07 12:48:25 +1100

    update lerp function

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def title(title_text):
 
 
 def lerp(number_a: float, number_b: float, ratio: float):
-    return int(number_a * ratio + number_b * (1 - ratio))
+    return int(number_b * ratio + number_a * (1 - ratio))
 
 
 def update_loudest_volume(new_volume: int):
@@ -33,6 +33,26 @@ def update_loudest_volume(new_volume: int):
             loudest_volume = new_volume
 
 
+def format_printed_volume_bar(new_volume: int, max_volume: int):
+    string_start = f"{new_volume:03}"
+    volume_area = '|' * new_volume
+
+    blank_spaces = max(total_line_spaces - new_volume, 0)  # IF VOLUME LARGER THAN 100, THIS WILL NOT BE NEGATIVE
+    blank_area = ' ' * blank_spaces
+
+    # DISPLAY MAX INDICATOR
+    if max_volume > new_volume:
+        gap_size = max_volume - new_volume - 1
+        gap_area = ' ' * gap_size
+
+        blank_spaces = max(total_line_spaces - max_volume - 1, 0)
+        blank_area = ' ' * blank_spaces
+
+        blank_area = f"{gap_area}❚{blank_area}"
+
+    return f"{string_start} {volume_area}{blank_area}"
+
+
 def display_information(new_volume: int):
     global previous_volume
 
@@ -42,9 +62,9 @@ def display_information(new_volume: int):
     if new_volume > previous_volume:
         lerped_new_volume = new_volume
     else:
-        lerped_new_volume = lerp(previous_volume, new_volume, 0.5)
+        lerped_new_volume = lerp(previous_volume, new_volume, 0.3)
 
-    print(f"{lerped_new_volume} {'|' * lerped_new_volume}{' ' * (max(total_line_spaces - lerped_new_volume, 0))}", end='\r')
+    print(format_printed_volume_bar(lerped_new_volume, loudest_volume), end='\r')
     previous_volume = new_volume
 
 
